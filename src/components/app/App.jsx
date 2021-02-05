1838a8fb2365ed71664318b47c2b7e076c5f46ab
Franco Ortega <francopdx@gmail.com>
2021-02-04 17:36:41 -0800

    add initial response field in Response.jsx and stylings in Response.css

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Request from '../requests/Request';
+import Response from '../response/Response';
 
 export default class App extends Component {
   render() {
     return (
       <>
         <Request />
+        <Response />
       </>
-    )
+    );
   }
 }
---
+++ src/components/response/Response.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './Response.css';
+
+export default class Response extends Component {
+  render() {
+    return (
+      <>
+        <section
+          className={styles.Response}
+        >
+            Response Field
+        </section>        
+      </>
+    );
+  }
+}
