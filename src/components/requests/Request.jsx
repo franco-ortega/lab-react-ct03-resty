8f889a385362fc8ce5a4ed52012ac75097ae645d
Franco Ortega <francopdx@gmail.com>
2021-02-04 17:24:19 -0800

    update form and add Request to App.jsx

--- src/components/requests/Request.jsx
+++ src/components/requests/Request.jsx
@@ -7,16 +7,16 @@ export default class Request extends Component {
         <form action="">
           <label htmlFor="endpoint">URL/Endpoint:</label>
           <input id="endpoint" type="text"/>
-
-          <label htmlFor="post">POST</label>
-          <input id="post" type="radio" name="POST"/>
-          <label htmlFor="get">GET</label>
-          <input id="get" type="radio" name="GET"/>
-          <label htmlFor="put">PUT</label>
-          <input id="put" type="radio" name="PUT"/>
-          <label htmlFor="delete">DELETE</label>
-          <input id="delete" type="radio" name="DELETE"/>
-
+          <p>
+            <label htmlFor="post">POST</label>
+            <input id="post" type="radio" name="method"/>
+            <label htmlFor="get">GET</label>
+            <input id="get" type="radio" name="method"/>
+            <label htmlFor="put">PUT</label>
+            <input id="put" type="radio" name="method"/>
+            <label htmlFor="delete">DELETE</label>
+            <input id="delete" type="radio" name="method"/>
+          </p>
           <label htmlFor="data">Enter Data:</label>
           <input id="data" type="text"/>
 
